a4481912393dd0af1c8c5a490016f739394ec571
Lil_Homer <55767500+Lil-Homer@users.noreply.github.com>
2021-12-28 18:10:55 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,61 @@
 from subprocess import check_output
+from os import system
+mensajes={"inicio":"INICIANDO EL GESTOR DE MODULOS PARA PYTHON",
+        "opcion":"Instalr/Desinstalar[I/D/B] --> ",
+        "modulo":"Modulo --> "
+          }
+
+def limpiar_pantalla():
+    system("cls")
+
+def comprobar_modulo(modulo):
+    try:
+        exec(f"import {modulo}")
+        return True
+    except Exception as e:
+        print(e)
+        return False
 def instalar(lista):
     no_inst=[]
     inst=[]
+    ya_inst=[]
     for modulo in lista:
-        try:
-            print(f">>> Iniciando instalación de: {modulo}")
-            consola=check_output("py -m pip install "+modulo,shell=True)
-            print(consola)
-            inst.append(modulo)
-        except Exception as e:
-            print(f"ERROR!:Fallo en la instalación de: {modulo}")
-            print("Exception:")
-            print(e)
-            no_inst.append(modulo)
-            
+        if comprobar_modulo(modulo):
+            print(f"{modulo} ya estaba instalado")
+            ya_inst.append(modulo)
+        else:
+            try:
+                print(f">>> Iniciando instalación de: {modulo}")
+                consola=check_output("py -m pip install "+modulo,shell=True)
+                print(consola)
+                
+            except Exception as e:
+                print(f"ERROR!:Fallo en la instalación de: {modulo}")
+                print("Exception:")
+                print(e)
+            if comprobar_modulo(modulo):
+                inst.append(modulo)
+            else:
+                no_inst.append(modulo)
         print("#"*50)
-    print(f"Modulos instalados correctamente: {len(inst)}/{len(lista)}:D")
-    for modulo in inst:
-        print(modulo)
+    print(f"MODULOS QUE YA ESTABAN INSTALADOS: {len(ya_inst)}/{len(lista)}:D")
+    print("_"*50)
+    for modulo in ya_inst:
+        print("-"+modulo)
+    print("\n")
     print("#"*50)
-    print(f"Modulos NO instalados: {len(no_inst)}/{len(lista)}:C")
+    print(f"MODULOS INSTALADOS CORRECTAMENTE: {len(inst)}/{len(lista)}:D")
+    print("_"*50)
+    for modulo in inst:
+        print("-"+modulo)
+    print("\n")
+    print("#"*50)
+    print(f"MODULOS QUE NO SE HAN INSTALADO: {len(no_inst)}/{len(lista)}:C")
+    print("_"*50)
     for modulo in no_inst:
-        print(modulo)
-
+        print("-"+modulo)
+    print("\n")
+    print("#"*50)
 def desinstalar(lista):
     no_inst=[]
     inst=[]
@@ -48,24 +81,31 @@ def desinstalar(lista):
         print(modulo)
 
 def p():
-    i=input("Instalr/Desinstalar[I/D] --> ")
+    print(mensajes["inicio"])
+    i=input(mensajes["opcion"])
     while i!="":
+        limpiar_pantalla()
         lista_modulos=[]
-        if i=="i":
-            t=input("Modulo --> ")
+        if i.lower()=="i":
+            print("INSTALAR UN MODULO")
+            t=input(mensajes["modulo"])
             while t!="":
                 lista_modulos.append(t)
                 t=input("Modulo --> ")
             print("INICIANDO INSTALACIÓN")
             print("#"*50)
             instalar(lista_modulos)
-        elif i=="d":
-            t=input("Modulo --> ")
+        elif i.lower()=="d":
+            print("DES-INSTALAR UN MODULO")
+            t=input(mensajes["modulo"])
             while t!="":
                 lista_modulos.append(t)
                 t=input("Modulo --> ")
             print("INICIANDO DESINSTALACIÓN")
             print("#"*50)
             desinstalar(lista_modulos)
-        i=input("Instalr/Desinstalar[I/D] --> ")
+        elif i.lower()=="b":
+            print("BUSCAR UN MODULO")
+            help('modules')
+        i=input(mensajes["opcion"])
 p()
